cf59d451c4d5355045dc80bd8725688f5e6ba91c
HanayoKoizumi <1478713180zeta@gmail.com>
2016-11-15 22:13:19 +0800

    回滚测试

--- Robocon_Test/USER/main.c
+++ Robocon_Test/USER/main.c
@@ -7,39 +7,39 @@
  */
  
  
- //由于核心板使用内部时钟，故屏蔽掉SystemInit
- 
- #include  "include.h"
-  float speed_set=0;
- float direction_set=0;
- uint32_t a,b,c=0;
- 
+//由于核心板使用内部时钟，故屏蔽掉SystemInit
 
- 
- int main()
- {
-
-	 
-
-	 RCC_Init();
-	 
-	 Robocon_PhoTriRacing_Board_Init();
-	 
-	 Robocon_NVIC_Init();
-	 
-	 DAC1_Init();
-	 
-	 setOrigin(0,0); 
-			 
-	 while(1)
-	 {
+#include  "include.h"
+float speed_set=0;
+float direction_set=0;
+uint32_t a,b,c=0;
 
 
-//printf("X_Angle：%.2f\r\nY_Angle：%.2f\r\nZ_Angle：%.2f\r\nX：%.2f\r\nY：%.2f\r\nZ_w：%.2f\r\n",motion.xangle,motion.yangle,motion.zangle,motion.pos_x,motion.pos_y,motion.w_z);
 
-	 } 
-		
- }
+int main()
+{
+
+
+
+	RCC_Init();
+
+	Robocon_PhoTriRacing_Board_Init();
+
+	Robocon_NVIC_Init();
+
+	DAC1_Init();
+
+	setOrigin(0,0); 
+		 
+	while(1)
+	{
+
+
+	//printf("X_Angle：%.2f\r\nY_Angle：%.2f\r\nZ_Angle：%.2f\r\nX：%.2f\r\nY：%.2f\r\nZ_w：%.2f\r\n",motion.xangle,motion.yangle,motion.zangle,motion.pos_x,motion.pos_y,motion.w_z);
+
+	} 
+
+}
  
 
 
